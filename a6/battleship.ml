499f972eaf4d9681da791de769489ee497af8af4
mrigney7 <mrigney7@gmail.com>
2019-04-09 19:21:05 -0400

    hits part of ship record

--- a6/battleship.ml
+++ a6/battleship.ml
@@ -4,14 +4,14 @@ type point = coordinate * status
 type grid = point list
 
 type name = Carrier | Battleship | Cruiser | Submarine | Destroyer  
-type ship = {name: name; size: int }
+type ship = {name: name; size: int; hits: int}
 
 
-let carrier = {name = Carrier; size = 5}
-let battleship = {name = Battleship; size = 4}
-let cruiser = {name = Cruiser; size = 3}
-let submarine = {name = Submarine; size = 3}
-let destroyer = {name = Destroyer; size = 2}
+let carrier = {name = Carrier; size = 5; hits = 0}
+let battleship = {name = Battleship; size = 4; hits = 0}
+let cruiser = {name = Cruiser; size = 3; hits = 0}
+let submarine = {name = Submarine; size = 3; hits = 0}
+let destroyer = {name = Destroyer; size = 2; hits = 0}
 
 let rows = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j']
 let columns = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
