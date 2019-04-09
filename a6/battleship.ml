c6b0ff47dc7766b543ce7d9b97579bf77da1d9f6
Kelly <ky356@cornell.edu>
2019-04-09 19:16:29 -0400

    rows cols

--- a6/battleship.ml
+++ a6/battleship.ml
@@ -1,7 +1,7 @@
 type coordinate = char * int 
 type status = Occupied | Hit | Empty  
 type point = coordinate * status
-type grid = point list 
+type grid = point list
 
 type name = Carrier | Battleship | Cruiser | Submarine | Destroyer  
 type ship = {name: name; size: int }
@@ -12,6 +12,11 @@ let battleship = {name = Battleship; size = 4}
 let cruiser = {name = Cruiser; size = 3}
 let submarine = {name = Submarine; size = 3}
 let destroyer = {name = Destroyer; size = 2}
+
+let rows = ['a';'b';'c';'d';'e';'f';'g';'h';'i';'j']
+let columns = [1; 2; 3; 4; 5; 6; 7; 8; 9; 10]
+
+
 
 
 
