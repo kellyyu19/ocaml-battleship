81fe620f78defa1fe2c291d268e9d568359e653c
mrigney7 <mrigney7@gmail.com>
2019-04-09 19:08:53 -0400

    ships

--- a6/battleship.ml
+++ a6/battleship.ml
@@ -1,3 +1,16 @@
 type coordinate = char * int 
 type status = Occupied | Hit | Empty  
 type point = coordinate * status
+
+type name = Carrier | Battleship | Cruiser | Submarine | Destroyer  
+type ship = {name: name; size: int }
+
+
+let carrier = {name = Carrier; size = 5}
+let battleship = {name = Battleship; size = 4}
+let cruiser = {name = Cruiser; size = 3}
+let submarine = {name = Submarine; size = 3}
+let destroyer = {name = Destroyer; size = 2}
+
+
+
