d55d4cc81c041a879f56cfb4c02ade4cbc9ad5cd
Moses Eboh <me363@cornell.edu>
2019-04-13 16:56:22 -0400

    main

--- a6/main.ml
+++ a6/main.ml
@@ -9,14 +9,18 @@ open Command
 
 
 
+let rec play_game_helper state =  
 
 
+  let play_game start = 
+    play_game_helper init_state
 
 
 let main () = 
-  ANSITerminal.print_string [ANSITerminal.Foreground Blue] "Test\n";
+  ANSITerminal.print_string [ANSITerminal.Foreground Blue] "\n Battleship\n";
+  print_endline "\n Please type start to play a new game.\n ";
   print_string ">>>";
-  match read_line () with 
-  |_ -> print_endline "unimplemented"
+  let start = read_line () in play_game start
+
 
 let () = main ()
