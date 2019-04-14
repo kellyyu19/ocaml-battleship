09dd88e2eecbcbbf8cd8961284d489e940253653
Moses Eboh <me363@cornell.edu>
2019-04-14 15:09:20 -0400

    main

--- a6/main.ml
+++ a6/main.ml
@@ -9,11 +9,25 @@ open Command
 
 
 
-let rec play_game_helper state =  
+let rec play_game_helper state_p1 state_p2 =  
+  if (placing state_p1) then 
+    ANSITerminal.print_string [ANSITerminal.Foreground Blue] 
+      "\n Player 1, please place your next ship. Ships remaining: " ^ queue state_p1;
+  play_game_helper (place state_p1) state_p2
+else if (placing state_p2) then
+  ANSITerminal.print_string [ANSITerminal.Foreground Blue] 
+    "\n Player 2, please place your next ship. Ships remaining: " ^ queue state_p1;
+play_game_helper (place state_p1) state_p2
+else 
+  try 
+  with 
+  | Malformed -> print_endline "\n That was not a valid command.\n";
+    play_game_helper state_p1 state_p2
 
 
-  let play_game start = 
-    play_game_helper init_state
+
+let play_game start = 
+  play_game_helper init_state init_state
 
 
 let main () = 
