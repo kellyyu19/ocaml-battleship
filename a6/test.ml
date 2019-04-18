65c6aa9fb424bdf8fb1a619ec3315461777b8f5a
mrigney7 <mrigney7@gmail.com>
2019-04-17 20:47:47 -0400

    new coords

--- a6/test.ml
+++ a6/test.ml
@@ -12,8 +12,8 @@ let rec get_point (coord:Battleship.coordinate) (grid: Battleship.grid) =
 
 
 let start_state = init_state 
-let carrier_placed = place (init_carrier) ('a',1) ('a',2) start_state
-let destroyer_placed = place (init_destroyer) ('b',1) ('b',2) carrier_placed
+let carrier_placed = place (init_carrier) ('a',1) ('b',1) start_state
+let destroyer_placed = place (init_destroyer) ('b',2) ('b',3) carrier_placed
 let submarine_placed = place (init_submarine) ('c',1) ('c',3) destroyer_placed
 let cruiser_placed = place (init_cruiser) ('d',1) ('d',3) submarine_placed
 let battleship_placed = place (init_battleship) ('e',1) ('e',4) cruiser_placed
@@ -26,6 +26,7 @@ let state_tests =
                                 (List.hd (carrier_fired1.current_grid)) );
     "testFireAtBattleship" >:: (fun _ -> assert_equal (('e', 3), Hit {name = Battleship; size = 4; hits = 1})
                                    (get_point ('e', 3) battleship_fired.current_grid) );
+    "
 
 
 
